9809ab474d575ec26bfa1a2598a3aeb439521b7a
s154666 <s154666@student.dtu.dk>
2020-03-25 17:04:44 +0100

    Committttttt

--- app/src/main/java/com/example/trackedraft3/MainActivity.kt
+++ app/src/main/java/com/example/trackedraft3/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.trackedraft3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_MESSAGE = "com.example.trackedraft3.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        middleButton.setOnClickListener{
+            val intent = Intent(this,Activity2::class.java)
+            startActivity(intent)
+        }
     }
 
     fun clickLeft(view: View){
@@ -18,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     fun clickRight(view: View){
         println("clicked right button")
     }
+
 
     fun clickMiddle(view: View){
         println("clicked middle button")
